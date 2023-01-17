1bcbcc0ae24eecf801313d1ccded629ae5e8b8c6
wtfdepartment <114874058+wtfdepartment@users.noreply.github.com>
2023-01-17 19:39:09 +0300

    Initial commit

---
+++ BankApp/BankApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankApp
+//
+//  Created by Александра on 17.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
