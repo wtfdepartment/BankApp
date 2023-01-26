e16bd32077f9dcae8a998ecaec6d7e69c06d960e
wtfdepartment <114874058+wtfdepartment@users.noreply.github.com>
2023-01-26 14:54:06 +0300

    Update MapController.swift

--- BankApp/BankApp/MapController.swift
+++ BankApp/BankApp/MapController.swift
@@ -8,24 +8,7 @@
 
 import UIKit
 
-<<<<<<< Updated upstream
-class MapController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-=======
 typealias LabelInfo = (String, UIColor)
 
 class MapController: UIViewController, GMSMapViewDelegate {
